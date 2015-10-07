9352924e3a611384335cd3e0437d9aab7bc17a84
Jamie Mclaughlan <jamie@jamiemc.co.uk>
2015-10-08 00:08:22 +0100

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -25,30 +25,45 @@ angular.module("nate.util", [])
     link: function(scope, element, attrs){
 
       $timeout(function(){
-      
+        
         scope.typeSpeed = scope.typeSpeed || 100;
         scope.loopDelay = scope.loopDelay || 2000;
         scope.cursor = scope.cursor || true;
-        
+        scope.loop = scope.loop || true;
+
         if(scope.cursor){
           var contentCursor = angular.element('<span class="cursor"> |</span>');
           contentCursor.insertAfter(element);
           $compile(contentCursor)(scope);
         }
+
+        var iconEndIndexs = [];
+        var count = 0;
         
         scope.typewrite_msgs = function(element, messages, messages_index, n, loop){
-          var end = n + 1;
-          var letter = messages[messages_index].substring(n,end);
 
-          if(n < messages[messages_index].length + 1){
-            if(letter === '<') {
-              var endIcon = messages[messages_index].indexOf('/>') + 2;
-              var icon = messages[messages_index].substring(n,endIcon);
-              element.html(messages[messages_index].substring(0,n));
-              element.append(icon);
+          var end = n + 1;
+          var sentence = messages[messages_index];
+          var singleLetter = sentence.substring(n,end);
+
+          if(n < sentence.length + 1){
+
+            // If start of html tag - perform magic
+            if(singleLetter === '<') {
+              for (var i = sentence.indexOf('/>'); i >= 0; i = sentence.indexOf('/>', i + 1)) {
+                iconEndIndexs.push(i);
+              }
+
+              var endIcon = iconEndIndexs[count] + 2;
+              var icon = sentence.substring(n, endIcon); // Find the icon in sentence 
+              element.html(sentence.substring(0,n)); // Get Everything up until icon...
+              element.append(icon); // then append the icon
+
+              count++;
             } else {
-              element.html(messages[messages_index].substring(0,n));
+              element.html(sentence.substring(0,n));
             }
+
             if(typeof icon === 'undefined') {
               $timeout(function(){
                 scope.typewrite_msgs(element, messages, messages_index, n+1, loop);
@@ -60,11 +75,15 @@ angular.module("nate.util", [])
             }
           }
           else if(messages_index+1 < messages.length){ // LOOPING BACK AROUND
+            iconEndIndexs = [];
+            count = 0;
             $timeout(function(){
               scope.typewrite_msgs(element, messages, messages_index+1, 0, loop);
             }, scope.loopDelay);
           }
           else if(scope.loop) {
+            iconEndIndexs = [];
+            count = 0;
             $timeout(function(){
               scope.typewrite_msgs(element, messages, 0, 0, loop);
             }, scope.loopDelay);
